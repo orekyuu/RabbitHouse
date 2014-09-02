480225354f9da5bdf08e6f165bbf666c79e42df7
orekyuu <orekyuu@yahoo.co.jp>
2014-09-02 17:18:50 +0900

    パスの修正

--- RabbitHouseTool/src/main/java/net/orekyuu/rabbithouse/util/FileDirs.java
+++ RabbitHouseTool/src/main/java/net/orekyuu/rabbithouse/util/FileDirs.java
@@ -8,11 +8,11 @@ public class FileDirs {
     }
 
     public static File createBlockDir(File root) {
-        return new File(root, "/net/orekyuu/rabbithouse/block/");
+        return new File(root, "/net/orekyuu/rabbithouse/loader/block/");
     }
 
     public static File createItemDir(File root) {
-        return new File(root, "/net/orekyuu/rabbithouse/item/");
+        return new File(root, "/net/orekyuu/rabbithouse/loader/item/");
     }
 
     public static File createBlockFile(File root) {
@@ -20,7 +20,7 @@ public class FileDirs {
     }
 
     public static File createItemFile(File root) {
-        return new File(root, "/net/orekyuu/rabbithouse/item/items.json");
+        return new File(root, "/net/orekyuu/rabbithouse/loader/item/Items.json");
     }
 
     public static File createSettingsFile(File root) {
