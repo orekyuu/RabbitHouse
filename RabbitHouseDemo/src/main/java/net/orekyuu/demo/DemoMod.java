8a30bd1775250645336d94a1458f6537702cfdff
orekyuu <orekyuu@yahoo.co.jp>
2014-09-02 17:11:53 +0900

    デモ用MODのプロジェクトを追加

---
+++ RabbitHouseDemo/src/main/java/net/orekyuu/demo/DemoMod.java
@@ -0,0 +1,25 @@
+package net.orekyuu.demo;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.block.Block;
+import net.orekyuu.rabbithouse.loader.block.BlockField;
+import net.orekyuu.rabbithouse.loader.block.BlockLoader;
+
+@Mod(modid = DemoMod.MODID)
+public class DemoMod {
+    public static final String MODID = "demo";
+    @Mod.Instance
+    public static DemoMod instance;
+
+    @BlockField(name = "test block")
+    public static Block block;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        BlockLoader loader = new BlockLoader(this, MODID);
+        loader.load();
+        System.out.println(block);
+    }
+
+}
